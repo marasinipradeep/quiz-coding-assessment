59a0e894c3416ba359db259d258a1027c7d3b677
marasinipradeep <marasinipradeep@gmail.com>
2020-06-08 22:29:40 +0930

    #Fixing UI and timer logic

--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -17,13 +17,6 @@ var optionEl = document.querySelector("#optionel")
 /* Getting elements to display result status*/
 var resultEl = document.querySelector("#result")
 
-
-
-
-
-
-
-
 let questions = [
   {
     id: 1,
@@ -85,9 +78,9 @@ let questions = [
   }
 
 ];
-var startingMinutes = 5;
+var startingMinutes = 1;
 var time = startingMinutes * 60;
-var startCount = setInterval(updateCountdown, 1000);
+//var startCount = setInterval(updateCountdown, 1000);
 
 
 
@@ -97,28 +90,32 @@ function onPlayClicked() {
   jumbboxEl.style.display = "block"
   playButton.style.display = "none"
   welcomeEl.textContent = "Coding Assessment"
-  startCount;
+  setInterval(updateCountdown, 1000)
   showQuestions(question_count);
 }
 
 
 
 function updateCountdown() {
+
   var minutes = Math.floor(time / 60);
   var seconds = time % 60;
-
   minutes = minutes < 10 ? "0" + minutes : minutes;
   seconds = seconds < 10 ? '0' + seconds : seconds
-  timerDisplay.textContent = minutes + ":" + seconds
-  // time --
-  if (--time < 0) {
+  timerDisplay.textContent = "Time left : " + minutes + ":" + seconds
+
+  time--
+
+  //if (timerDisplay.textContent === "00:00") {
+    if (time < 0) {
+
     stopTimer();
   }
 }
 
 /* When time reaches to 0 clears the execution of setInterval*/
 function stopTimer() {
-  clearInterval(startCount)
+  clearInterval(updateCountdown)
   openSummaryPage();
 
 }
@@ -130,74 +127,71 @@ var correct_answer = 0;
 function showQuestions(count) {
 
   questionEl.textContent = questions[count].question
-  optionEl.textContent =""
+  optionEl.textContent = ""
+
   for (i = 0; i < questions[count].options.length; i++) {
     var li = document.createElement("li");
-    
     li.setAttribute("class", "option")
     li.setAttribute("data-index", i)
+    li.style.fontSize = "20px"
+    li.style.color = "blue"
+    // li.style.position = 'relative';// ask here
+    // li.style.top = "100px"// ask here
     li.textContent = questions[count].options[i];
     optionEl.appendChild(li);
   }
 
-  keepActive()
+  // keepActive()
 
 }
 
 
-function keepActive() {
-  let option = document.querySelectorAll("li.option");
-  for (let i = 0; i < option.length; i++) {
-    option[i].onclick = function () {
-      for (let i = 0; i < option.length; i++) {
-        if (option[i].classList.contains("active")) {
-          option[i].classList.remove("active");
-        }
-      }
-      option[i].classList.add("active");
-    };
-  }
+// function keepActive() {
 
-}
+//   setTimeout(function(){
+//   let option = document.querySelectorAll("li.option");
+//   for (let i = 0; i < option.length; i++) {
+//     option[i].onclick = function () {
+//       for (let i = 0; i < option.length; i++) {
+//         if (option[i].classList.contains("active")) {
+//           option[i].classList.remove("active");
+//         }
+//       }
+//       option[i].classList.add("active");
+//     };
+//   }
+
+// },3000)}
 
 optionEl.addEventListener("click", function (event) {
-
   console.log("inside check answer")
   var element = event.target;
-
   if (element.matches("li") === true) {
-
     var index = element.getAttribute("data-index");
-    console.log("data index is :" +index);
+    console.log("data index is :" + index);
     console.log("option is :" + questions[question_count].options[index]);
-
     if (questions[question_count].options[index] == questions[question_count].answer) {
       console.log("your point before loop :" + questions[0].options[index]);
+      //resultEl.style.display = "block"
       resultEl.textContent = "Correct answer"
       correct_answer++
       localStorage.setItem("correct_answer", correct_answer)
-     
-      }
-    else{
-      resultEl.textContent = "Wrong answer"
     }
+    else { 
+      resultEl.textContent = "Wrong answer time deducted by 30 seconds"
+      time = time - 30
+      return
+     }
 
-  }
-  else {
-    console.log("No options selected")
-  }
+  } else { console.log("No options selected") }
 
-  if(questions[question_count].id===5){
+  if (questions[question_count].id === 5) {
     openSummaryPage();
   }
-
   question_count++
-  console.log("Question count"+questions[question_count].id)
+  console.log("Question count" + questions[question_count].id)
   showQuestions(question_count);
-
 })
-
-
 
 
 function openSummaryPage() {
